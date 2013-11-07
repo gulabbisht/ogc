055e9a531738dc5ef3b2b7b852d156ac20c7b689
Gulab Bisht <gulab.bisht@gmail.com>
2013-11-07 09:37:15 +0530

    Module and tpl file added.

---
+++ ogc_comment.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Theme implementation for open graph comment.
+ * 
+ */
+?>
+<div>
+  <div style="float: left; padding: 10px;">
+    <img style="width: 125px;" src='<?php print $img; ?>'>
+  </div>
+    <h3><a href="<?php print $url; ?>" target="_blank"><?php print $title; ?></a></h3>
+    <p><?php print $desc ?></p>
+  <div>
+  </div>
+</div>
