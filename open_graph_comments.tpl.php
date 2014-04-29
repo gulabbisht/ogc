23ed6f68827953ef41b91eabce61543e21628ad3
gulab.bisht <gulab.bisht@gmail.com>
2014-04-29 19:40:08 +0530

    Changes and content type based configuration added.

--- open_graph_comments.tpl.php
+++ open_graph_comments.tpl.php
@@ -5,12 +5,12 @@
  * Theme implementation for open graph comment.
  */
 ?>
-<div>
-  <div style="float: left; padding: 10px;">
-    <img style="width: 125px;" src='<?php print $img; ?>'>
+<div class="open-graph-comment-outer">
+  <div class="open-graph-comment-img">
+    <img src='<?php print $img; ?>'>
   </div>
-    <h3><a href="<?php print $url; ?>" target="_blank"><?php print $title; ?></a></h3>
-    <p><?php print $desc ?></p>
+    <h3 class="open-graph-comment-title"><a href="<?php print $url; ?>" target="_blank"><?php print $title; ?></a></h3>
+    <p class="open-graph-comment-desc"><?php print $desc ?></p>
   <div>
   </div>
 </div>
