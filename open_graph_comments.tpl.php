5fa33688030c96f0a73a4850a76757b1c9feb2c8
Gulab Bisht <gulab.bisht@gmail.com>
2014-07-08 17:43:15 +0530

    Module and tpl files modified for previous commit.

--- open_graph_comments.tpl.php
+++ open_graph_comments.tpl.php
@@ -7,10 +7,10 @@
 ?>
 <div class="open-graph-comment-outer">
   <div class="open-graph-comment-img">
-    <img src='<?php print $img; ?>'>
+    <img src='<?php print $og_data['img']; ?>'>
   </div>
-    <h3 class="open-graph-comment-title"><a href="<?php print $url; ?>" target="_blank"><?php print $title; ?></a></h3>
-    <p class="open-graph-comment-desc"><?php print $desc ?></p>
+    <h3 class="open-graph-comment-title"><a href="<?php print $og_data['url']; ?>" target="_blank"><?php print $og_data['title']; ?></a></h3>
+    <p class="open-graph-comment-desc"><?php print $og_data['desc']; ?></p>
   <div>
   </div>
 </div>
